2d153997996bb3e10d820458bdfd1e240d8339a6
Nyamadi Atsu <devbrainy98@gmail.com>
2023-06-08 11:21:43 +0100

    started policy page

--- src/app/policy/page.tsx
+++ src/app/policy/page.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import HeroComponent from '../components/HeroComponent'
+import heroBg from '../../assets/policyhero.webp'
+
+export const metadata = {
+  title: 'Our Policies | JFSRC'
+}
 
 const Policy = () => {
   return (
-    <div>Policy</div>
+    <section>
+      <HeroComponent 
+        image={heroBg}
+        route='Policy'
+      >
+        <span className='text-red-main'>JFSRC</span>
+        <span>Policies</span>
+      </HeroComponent>
+    </section>
   )
 }
 
